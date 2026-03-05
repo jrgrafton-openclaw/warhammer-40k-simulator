1e56966f0a30c174380779de148f93a279166ee1
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-05 05:17:22 +0000

    chore: deploy f674d5da850c6b4f4b8f7a5c69baf6bc1164d52b f674d5da850c6b4f4b8f7a5c69baf6bc1164d52b

--- assets/BufferResource-B__2xk6t.js
+++ assets/BufferResource-B__2xk6t.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-CJa_J7_a.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Bk6a0beg.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
