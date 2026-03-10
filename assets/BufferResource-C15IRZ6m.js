170b9d6f60297d70406d3d9f16e83d00840fd3fe
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-10 00:50:21 +0000

    chore: deploy 6b047d20285f970d3e17b7867cd8d24df7313a00 6b047d20285f970d3e17b7867cd8d24df7313a00

--- assets/BufferResource-C15IRZ6m.js
+++ assets/BufferResource-C15IRZ6m.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BlowtTai.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-B9nT1-My.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
