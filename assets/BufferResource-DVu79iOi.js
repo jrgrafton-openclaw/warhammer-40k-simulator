3289b83946c12297c07a63b0bf3000b3179164a9
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-12 04:22:01 +0000

    chore: deploy c4d59c2cf3f8a056baebcadad5873a985be1e03e c4d59c2cf3f8a056baebcadad5873a985be1e03e

--- assets/BufferResource-DVu79iOi.js
+++ assets/BufferResource-DVu79iOi.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-BjVQ0nB_.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Cc3tYPCU.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
