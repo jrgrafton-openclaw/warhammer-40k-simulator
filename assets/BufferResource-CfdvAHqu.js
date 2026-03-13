f72f5f1bca0534f16234846b2c4f5c5bc2f238d8
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-13 04:10:46 +0000

    chore: deploy 2047b59098eb855bdeb4ec1af168ee8626434eb6 2047b59098eb855bdeb4ec1af168ee8626434eb6

--- assets/BufferResource-CfdvAHqu.js
+++ assets/BufferResource-CfdvAHqu.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-D4wiO7py.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-CBwNv4yq.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
