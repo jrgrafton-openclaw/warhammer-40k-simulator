57c4d91ab1b66704375a154bec58c3c0a563bb6c
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-14 21:03:02 +0000

    chore: deploy 0c3797ee8c795b781750fae22e991efa0d0329d8 0c3797ee8c795b781750fae22e991efa0d0329d8

--- assets/BufferResource-Cn57IEVQ.js
+++ assets/BufferResource-Cn57IEVQ.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-CjEld4Tq.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Bb_rQ6dA.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
