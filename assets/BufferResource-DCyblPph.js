536c85bda0dbd06822489f2125b911d8ab7b37f0
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-15 15:10:03 +0000

    chore: deploy 76d41350c92dc1c002004fe3be2914a8de6d8c89 76d41350c92dc1c002004fe3be2914a8de6d8c89

--- assets/BufferResource-DCyblPph.js
+++ assets/BufferResource-DCyblPph.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Dtg6M7YZ.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-DsB1EHOK.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
