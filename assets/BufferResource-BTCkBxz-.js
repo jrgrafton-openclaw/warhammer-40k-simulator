82de5a3b6e1b09e9854e1b26cf91d31d501fe991
jrgrafton-openclaw <jrgrafton-openclaw@users.noreply.github.com>
2026-03-15 23:50:45 +0000

    chore: deploy 08313e6987cae8596abe1b5ff6b3f64662403a59 08313e6987cae8596abe1b5ff6b3f64662403a59

--- assets/BufferResource-BTCkBxz-.js
+++ assets/BufferResource-BTCkBxz-.js
@@ -1,4 +1,4 @@
-import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-fOu0L1QD.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,g,c,u as S,B as _,d as m,E as I,e as p}from"./index-Bjbr4gAJ.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
